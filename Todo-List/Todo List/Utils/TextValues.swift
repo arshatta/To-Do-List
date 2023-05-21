34925cc32cdf39d3244f97030f6fa6f9cb48af37
Talap Arshat <103632834+arshatta@users.noreply.github.com>
2023-05-21 15:12:05 +0600

    Add files via upload

---
+++ Todo-List/Todo List/Utils/TextValues.swift
@@ -0,0 +1,16 @@
+//
+//  TextValues.swift
+//  Todo List
+//
+//  Created by Javier Cueto on 10/04/22.
+//
+
+import Foundation
+
+enum TextValues {
+    static let descriptionPlaceHolder = "Write task..."
+    static let saveButton = "Save"
+    static let titleNewTodo = "New Todo"
+    static let titleList = "List of Todo"
+    static let empty = ""
+}
